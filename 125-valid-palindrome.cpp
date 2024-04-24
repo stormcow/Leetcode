d04f97d46a6d74722a3293c8ba5c8d9a13799e00
stormcow <monkiimode@gmail.com>
2024-04-24 22:10:34 +0200

    up

--- 125-valid-palindrome.cpp
+++ 125-valid-palindrome.cpp
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-#include <string>
-#include <algorithm>
-#include <cwctype>
-#include <cctype>
-#include <iostream>
-
-    void convert(std::string& s){
-        
-        for(int i = 0; i < s.size(); i++){
-            if(!iswalnum(s[i]))
-                s.erase(s.begin()+i);
-            else
-                s[i]=std::tolower(s[i]);
-        }
-        
-    }
-
-        bool isPalindrome(std::string s) {
-        convert(s);
-        std::string b = s;
-        std::reverse(b.begin(),b.end());
-        if(b==s)
-            return true;
-        return false;
-    }
-
-    int main(){
-        std::string s = "A man, a plan, a canal: Panama";
-
-        std::cout<<isPalindrome(s);
-    }
-=======
 #include <algorithm>
 #include <cctype>
 #include <cwctype>
@@ -56,4 +23,3 @@ public:
     return false;
   }
 };
->>>>>>> 68f9a0b2470b6ba5d15210368b05878f9b1c7856
--- 567-permutation-in-string.py
+++ 567-permutation-in-string.py
@@ -1,26 +1,21 @@
-def permutate(s1: str, s2: str) -> bool:
-    if len(s1) > len(s2):
-        return False
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
 
-    if s1 == s2:
-        return True
+        sorted_s1 = "".join(sorted(s1))
 
-    sorted_s1 = "".join(sorted(s1))
+        to_check = "".join(sorted(s2[: len(s1)]))
+        rest = s2[1:]
 
-    to_check = "".join(sorted(s2[: len(s1)]))
-    rest = s2[1:]
-
-    while rest != "":
         if to_check == sorted_s1:
             return True
-        else:
-            to_check = "".join(sorted(rest[: len(s1)]))
-            rest = rest[1 :]
 
-    return False
+        while rest != "":
+            if to_check == sorted_s1:
+                return True
+            else:
+                to_check = "".join(sorted(rest[: len(s1)]))
+                rest = rest[1:]
 
-
-if permutate("abc", "bbbca"):
-    print("permutates")
-else:
-    print("doesnt permuate")
+        return False
